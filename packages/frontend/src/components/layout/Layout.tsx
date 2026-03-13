0f3ace708a453eaeda5ddfe999e4826108e20834
Brandon Grabowski <83241992+Brandon82@users.noreply.github.com>
2026-03-12 23:48:04 -0400

    Center guide content in main layout

--- packages/frontend/src/components/layout/Layout.tsx
+++ packages/frontend/src/components/layout/Layout.tsx
@@ -8,8 +8,10 @@ export function Layout() {
       <Header />
       <div className="flex flex-1 overflow-hidden">
         <Sidebar />
-        <main className="flex-1 overflow-y-auto p-6">
-          <Outlet />
+        <main className="flex-1 overflow-y-auto p-6 flex justify-center">
+          <div className="w-full max-w-3xl">
+            <Outlet />
+          </div>
         </main>
       </div>
     </div>
